7b654287b58bb78a3a930bd16a34ad90ed9d5317
Chris-Tate94 <chris_tate_94@hotmail.com>
2022-06-29 18:25:12 -0400

    added swiper js

--- portfolio/src/components/testimonials/Testimonials.jsx
+++ portfolio/src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/AVTR1.jpg'
+import AVTR2 from '../../assets/AVTR2.jpg'
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Client Name / Skills',
+    review: 'Client Review / Skill Description / Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem hic rerum nobis odit, ab, cupiditate exercitationem iste error quibusdam aspernatur, labore nemo debitis praesentium esse suscipit odio. Tenetur, ea beatae.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Client Name / Skills 2',
+    review: 'Client Review / Skill Description / Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem hic rerum nobis odit, ab, cupiditate exercitationem iste error quibusdam aspernatur, labore nemo debitis praesentium esse suscipit odio. Tenetur, ea beatae.'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Client Name / Skills 3',
+    review: 'Client Review / Skill Description / Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem hic rerum nobis odit, ab, cupiditate exercitationem iste error quibusdam aspernatur, labore nemo debitis praesentium esse suscipit odio. Tenetur, ea beatae.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Client Name / Skills 4',
+    review: 'Client Review / Skill Description / Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem hic rerum nobis odit, ab, cupiditate exercitationem iste error quibusdam aspernatur, labore nemo debitis praesentium esse suscipit odio. Tenetur, ea beatae.'
+  },
+]
+
+
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt="Avatar" />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
