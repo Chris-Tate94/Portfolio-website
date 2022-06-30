142267970ea0ac96716722d71013baf986b186d8
Chris-Tate94 <chris_tate_94@hotmail.com>
2022-06-29 22:32:01 -0400

    added testimonial and contact details

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsTelephone } from "react-icons/bs";
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>Chris_tate_94@hotmail.com</h5>
+            <a href='mailto:chris_tate_94@hotmail.com'>Email Me</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>Chris Tate</h5>
+            <a href='https://m.me/chris.tate.925'>Message Me</a>
+          </article>
+          <article className='contact__option'>
+            <BsTelephone />
+            <h4>Call</h4>
+            <h5>+1(647)970-3677</h5>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
--- portfolio/src/components/header/HeaderSocials.jsx
+++ portfolio/src/components/header/HeaderSocials.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { FiDribbble } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href='https://www.linkedin.com/in/christopher-tate-77a7241a4/' target='__blank'><BsLinkedin /></a>
         <a href='https://github.com/Chris-Tate94' target='__blank'><BsGithub /></a>
-        <a href='https://github.https://dribbble.com/ctate69?onboarding=true/Chris-Tate94' target='__blank'><FiDribbble /></a>
     </div>
   )
 }
