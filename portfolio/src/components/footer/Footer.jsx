87f64b94274fc3d779caf3045613eeaf86438398
Chris-Tate94 <chris_tate_94@hotmail.com>
2022-07-01 19:52:47 -0400

    added perma links and copyright

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './footer.css'
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>LOGO</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://facebook.com"> <BsFacebook/> </a>
+        <a href="https://instagram.com"> <BsInstagram/> </a>
+        <a href="https://linkedin.com"> <BsLinkedin/> </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Chris Tate. All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
