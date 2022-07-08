2277aa3b5dce8eaf0ae97021fb793bae7becdbb7
Chris-Tate94 <chris_tate_94@hotmail.com>
2022-07-08 15:30:22 -0400

    minor changes in header

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CTA from './CTA'
 import "./header.css"
-import Picture from '../../assets/profile.JPG'
+import picture from '../../assets/profile.JPG'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -10,12 +10,12 @@ const Header = () => {
       <div  className='container header__container'>
         {/* <h5>Hello, my name is</h5> */}
         <h1>Chris Tate</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
 
         <div className='picture'>
-          <img src={Picture} alt="picture" />
+          <img src={picture} alt="picture" />
         </div>
 
         <a href='#contact' className='scroll__down'>Scroll Down</a>
